b44829bbeb0c79990303192fe8a46be1bcbb5977
bobbysam <bobby.sambangi@thetiebar.com>
2017-05-22 17:47:06 -0500

    final

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+ $(".btn").click(function () {
+
+        $(".toggle").slideToggle("slow")
+    });
+   
+  $(".bottom-left-data-1").hide();
+  
+     $(".btn1").click(function () {
+       
+
+         $(".Originalheaders1").html("<h1>TIES</h1>");
+         $(".OriginalSubheaders1").html(" <h3>Sub-Ties</h3>");
+         $(".OriginalParagraphs1").html(" <p>Checkout the new bowTies</p>");
+
+            $(".Originalheaders2").html("<h1>PSQUARES</h1>");
+         $(".OriginalSubheaders2").html("<h3>PocketSquares</h3>");
+         $(".OriginalParagraphs2").html(" <p>Check out our new Pocket squares</p>");
+
+            $(".Originalheaders3").html(" <h1>BowTies</h1>");
+         $(".OriginalSubheaders3").html(" <h3>SubBow-Ties</h3>");
+         $(".OriginalParagraphs3").html("  <p>Checkout the new bowTies</p>");
+         
+
+            $(".Originalheaders4").html(" <h1>TIEBARS</h1>");
+         $(".OriginalSubheaders4").html("  <h3>ExtraLarge-Ties</h3>");
+         $(".OriginalParagraphs4").html(" <p>Checkout the new Ties</p>");
+
+         
+         
+
+     
+        
+        });
